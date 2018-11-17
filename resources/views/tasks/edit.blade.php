6477ce44d26570316966351f9f1ec1e7d3db7a2b
ryo.shibata <RYO.liverpro@gmail.com>
2018-11-17 09:03:30 +0000

    bootstrap design

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -8,6 +8,7 @@
 <div class="row">
         <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-sm-8 col-lg-offset-3 col-sm-6">
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+    <input type="text" class="form-control">
     </div>
 
 <div class="form-group">
@@ -18,6 +19,7 @@
         <div class="form-group">
         {!! Form::label('content', 'タスク:') !!}
         {!! Form::text('content', null, ['class' => 'form-control']) !!}
+        
         </div>
 
                 {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
