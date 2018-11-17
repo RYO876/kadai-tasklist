1a6dd0aef97cc5b766f5b15958eacc0a81509b81
ryo.shibata <RYO.liverpro@gmail.com>
2018-11-17 04:40:27 +0000

    bootstrap design

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -10,7 +10,7 @@
             <thead>
                 <tr>
                     <th>id</th>
-                    <th>タイトル</th>
+                    <th>ステータス</th>
                     <th>タスク</th>
                 </tr>
             </thead>
